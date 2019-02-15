14ad67e6bc6e9b0ead27cde770e94176f262c2e0
Wellington-Yoshdia <issami-yoshida@hotmail.com>
2019-02-15 21:06:06 -0200

    Criando projeto Sistema para Pizzaria

---
+++ src/main/java/br/com/sistemapizzaria/SistemaparaPizzaria/SistemaParaPizzariaApplication.java
@@ -0,0 +1,14 @@
+package br.com.sistemapizzaria.SistemaparaPizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaParaPizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaParaPizzariaApplication.class, args);
+	}
+
+}
+
